8c6a17cd4e9e36c20233f85defe64e554302398d
bneveret <eve20003@byui.edu>
2022-02-05 13:27:24 -0700

    chamber updated

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -6,12 +6,18 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 x.onclick= toggleMenu;
 
-const lastmod = document.querySelector("#lastmod")
-lastmod.textContent = "Last Modification: " + document.lastModified
+const lastmod = document.querySelector("#lastmod");
+lastmod.textContent = "Last Modification: " + document.lastModified;
 
-const currentdate = document.querySelector("#currentdate")
-const d = new Date()
-month = d.getMonth() + 1
-day = d.getDate()
-year = d.getFullYear()
-currentdate.textContent = month.toString() +'/'+ day.toString() +'/'+ year.toString()
+const currentdate = document.querySelector("#currentdate");
+const d = new Date();
+month = d.getMonth() + 1;
+day = d.getDate();
+year = d.getFullYear();
+currentdate.textContent = month.toString() +'/'+ day.toString() +'/'+ year.toString();
+
+const banner = document.querySelector('#banner');
+weekday = d.getDay();
+if (weekday == 0 || weekday == 3 || weekday == 4 || weekday == 5 || weekday == 6){
+    banner.classList.toggle("closed");
+};
