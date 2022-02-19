b92773b9ab2180df408771080140d9af491c8a5e
bneveret <eve20003@byui.edu>
2022-02-19 10:57:19 -0700

    small

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -11,21 +11,21 @@ lastmod.textContent = "Last Modification: " + document.lastModified;
 
 const currentdate = document.querySelector("#currentdate");
 const d = new Date();
-month = d.getMonth() + 1;
-day = d.getDate();
-year = d.getFullYear();
+var month = d.getMonth() + 1;
+var day = d.getDate();
+var year = d.getFullYear();
 currentdate.innerHTML = month.toString() +'/'+ day.toString() +'/'+ year.toString();
 
 const banner = document.querySelector('#banner');
-weekday = d.getDay();
+var weekday = d.getDay();
 if (weekday == 0 || weekday == 3 || weekday == 4 || weekday == 5 || weekday == 6){
     banner.classList.toggle("closed");
 };
 
 function pagedays() {
-    date2 = d.getTime()
-    date1 = JSON.parse(window.localStorage.getItem('date1'))
-    timeDifference = (date2 - date1)/(1000 * 3600 * 24);
+    var date2 = d.getTime()
+    var date1 = JSON.parse(window.localStorage.getItem('date1'))
+    var timeDifference = (date2 - date1)/(1000 * 3600 * 24);
     window.localStorage.setItem('date1', JSON.stringify(date1));
     document.querySelector('#days').innerHTML = 'Days Since Last Visit: ' + timeDifference;
 }
