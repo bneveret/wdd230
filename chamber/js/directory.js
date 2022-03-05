9c45d35a040716417b82a23a5de1340318b2a2b7
bneveret <eve20003@byui.edu>
2022-03-05 14:49:53 -0700

    directory draft 1

---
+++ chamber/js/directory.js
@@ -0,0 +1,34 @@
+const requestURL = 'data/data.json';
+const cards = document.querySelector('.cards');
+const list = document.querySelector('.list');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+      const companies = jsonObject['companies'];
+      companies.forEach(displayCards);
+      //companies.forEach(displayList);
+  });
+
+function displayCards(company) {
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let site = document.createElement('p');
+
+    logo.setAttribute('src', company.logourl);
+
+    address.textContent = company.address;
+    phone.textContent = company.phone;
+    site.textContent = company.site;
+    
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(site);
+
+    cards.appendChild(card);
+}
