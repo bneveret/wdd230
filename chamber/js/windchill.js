20ee9541fcf13a9f50d2c4325afd946913308a7b
bneveret <eve20003@byui.edu>
2022-03-12 16:22:47 -0700

    fixed api link

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,4 +1,4 @@
-const OWAPI = "http://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&appid=846857d20f89e974635e846ef8d07860";
+const OWAPI = "https://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&appid=846857d20f89e974635e846ef8d07860";
 fetch(OWAPI)
 .then((response) => response.json())
 .then((jsObject) => {
